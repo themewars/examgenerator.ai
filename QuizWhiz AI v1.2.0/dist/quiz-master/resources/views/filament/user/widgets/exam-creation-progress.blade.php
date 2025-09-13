c929f052326632cc193f0a7d3346485f1b3b36d7
pagalworldmp3 <90148815+pagalworldmp3@users.noreply.github.com>
2025-09-13 23:40:42 +0530

    🚨 CRITICAL FIX: Completely remove ExamCreationProgress widget
    - DELETED ExamCreationProgress.php widget completely
    - DELETED all exam-creation-progress.blade.php files
    - This should completely fix the 500 server error
    - Widget was being auto-discovered by Filament
    - No more Livewire errors from this widget

--- QuizWhiz AI v1.2.0/dist/quiz-master/resources/views/filament/user/widgets/exam-creation-progress.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- Progress widget disabled - no longer needed --}}
-{{-- This widget previously showed exam creation progress --}}
-{{-- Now disabled to prevent progress bar from showing --}}
-
-<div class="bg-white rounded-lg shadow-sm border border-gray-200 p-6">
-    <div class="text-center text-gray-500">
-        <p>Progress widget disabled</p>
-    </div>
-</div>
